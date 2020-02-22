126d668cabc73dc85a5560b5da0d76fa3bafa05e
kuwana-kb <41671293+kuwana-kb@users.noreply.github.com>
2020-02-23 00:27:10 +0900

    Add domain service

--- src/c3_entity.rs
+++ src/c3_entity.rs
@@ -1,3 +1,5 @@
 mod a1_simple_user;
 mod a2_mutable_entity;
 mod a3_identify_entity;
+
+pub use a3_identify_entity::*;
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate anyhow;
 
 mod c2_value_object;
 mod c3_entity;
+mod c4_domain_service;
 mod error;
 
 pub use error::*;
