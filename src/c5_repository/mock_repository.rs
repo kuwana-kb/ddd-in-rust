f87d941299abec9dc76c51f39b0c6cfd1d4555b2
kuwana-kb <41671293+kuwana-kb@users.noreply.github.com>
2020-02-26 22:59:39 +0900

    Refactor MockRepository to use Arc<Mutext<_>>

--- src/c5_repository/mock_repository.rs
+++ src/c5_repository/mock_repository.rs
@@ -1,30 +1,36 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
 use derive_new::new;
 
 use super::{IUserRepository, Name, User, UserId};
+use crate::MyError;
 
 #[derive(Clone, new)]
 pub struct InMemoryUserRepository {
-    store: HashMap<UserId, User>,
+    store: Arc<Mutex<HashMap<UserId, User>>>,
 }
 
 impl IUserRepository for InMemoryUserRepository {
-    // TODO: ここの実装をどうするか検討する
-    // Mockは内部のHashMapにデータを保存する形にしたい
-    // しかし、そうするとsaveメソッドのselfをmutにする必要がでてくる。これはinterfaceに影響を与えてしまうため要検討
-    fn save(&mut self, user: User) -> Result<()> {
+    fn save(&self, user: User) -> Result<()> {
         println!("save: {:?}", user);
-        self.store.insert(user.id().clone(), user);
+        let store = self.store.clone();
+        let mut store = store
+            .try_lock()
+            .map_err(|_| MyError::internal_server_error("failed to try_lock store"))?;
+        store.insert(user.id().clone(), user);
         Ok(())
     }
 
     fn find(&self, name: Name) -> Result<Option<User>> {
-        let target = self
-            .store
+        let store = self.store.clone();
+        let store = store
+            .try_lock()
+            .map_err(|_| MyError::internal_server_error("failed to try_lock store"))?;
+        let target = store
             .values()
-            // .filter(|user| user.name().clone() == name)
+            .filter(|user| user.name().clone() == name)
             .cloned()
             .collect::<Vec<User>>();
         println!("target: {:?}", target);
@@ -36,7 +42,7 @@ impl IUserRepository for InMemoryUserRepository {
 fn test_mock_repostitory() {
     use super::Program;
 
-    let repo = InMemoryUserRepository::new(HashMap::new());
+    let repo = InMemoryUserRepository::new(Arc::new(Mutex::new(HashMap::new())));
     let mut program = Program::new(repo);
     program.create_user("Hoge".parse().unwrap()).unwrap();
     let opt_user = program.repo().find("Hoge".parse().unwrap()).unwrap();
