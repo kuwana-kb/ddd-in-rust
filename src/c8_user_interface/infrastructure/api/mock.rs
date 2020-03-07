04652cb64375f802f2c45167896ef8ffd6e30204
kuwana-kb <41671293+kuwana-kb@users.noreply.github.com>
2020-03-08 00:59:01 +0900

    Define api

---
+++ src/c8_user_interface/infrastructure/api/mock.rs
@@ -0,0 +1,23 @@
+use crate::c8_user_interface::{
+    domain::HaveUserRepository, infrastructure::MockContext, usecase::HaveUserApplicationService,
+};
+
+pub struct MockApi {
+    context: MockContext,
+}
+
+impl HaveUserRepository for MockApi {
+    type UserRepository = MockContext;
+
+    fn provide_user_repository(&self) -> &Self::UserRepository {
+        &self.context
+    }
+}
+
+impl HaveUserApplicationService for MockApi {
+    type UserApplicationService = Self;
+
+    fn provide_user_service(&self) -> &Self::UserApplicationService {
+        self
+    }
+}
