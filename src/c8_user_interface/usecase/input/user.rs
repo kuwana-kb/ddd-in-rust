ee743813b868ebd46cf068beeb87cca8094c2232
kuwana-kb <41671293+kuwana-kb@users.noreply.github.com>
2020-03-08 00:57:30 +0900

    Refactor ApplicationService.

--- src/c8_user_interface/usecase/input/user.rs
+++ src/c8_user_interface/usecase/input/user.rs
@@ -1,17 +1,24 @@
 use derive_getters::Getters;
 use derive_new::new;
+use serde::Deserialize;
 
-use crate::c8_user_interface::{domain::UserId, MailAddress, Name};
+use crate::c8_user_interface::domain::{MailAddress, Name, UserId};
+
+#[derive(Clone, Debug, Getters, new, Deserialize)]
+pub struct CreateUserCommand {
+    name: Name,
+    mail_address: MailAddress,
+}
 
 // Updateメソッドのパラメータ群。Optionで定義したフィールドは任意の更新項目とする。
 #[derive(Clone, Debug, Getters, new)]
-pub struct UserUpdateCommand {
+pub struct UpdateUserCommand {
     id: UserId,
     name: Option<Name>,
     mail_address: Option<MailAddress>,
 }
 
 #[derive(Clone, Debug, Getters, new)]
-pub struct UserDeleteCommand {
+pub struct DeleteUserCommand {
     id: UserId,
 }
